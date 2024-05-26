87405e5e82fe50b42074698e7a8f8da3fca0c2ae
Lalov Kirill <lalovkir@gmail.com>
2024-05-26 14:03:18 +0300

    Добавлен hook useCounter

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
     alias: {
       '@app': resolve(__dirname, 'src', 'app'),
       '@components': resolve(__dirname, 'src', 'components'),
+      '@hooks': resolve(__dirname, 'src', 'hooks'),
     },
   },
 });
